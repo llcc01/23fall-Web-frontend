4bb8aeeb1f929fc99022fa7ca359decb5a900d52
llcc01 <2417224420@qq.com>
2023-12-31 01:14:53 +0800

    add chat

--- src/pages/message.tsx
+++ src/pages/message.tsx
@@ -1,77 +1,70 @@
-import { Layout, List } from "@douyinfe/semi-ui";
+import { Button, Form, Layout, List, Modal } from "@douyinfe/semi-ui";
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { User } from "../../types";
+import { Message, User } from "../../types";
 import Chat, { Bubble, useMessages } from "@chatui/core";
 
 import "@chatui/core/es/styles/index.less";
 import type { MessageProps } from "@chatui/core";
 import "@chatui/core/dist/index.css";
-import "../chatui-theme.css"
+import "../chatui-theme.css";
+import { useAuth } from "../utils/auth";
+import { getUserInfo } from "../utils/userInfo";
+import { useNavigate } from "react-router-dom";
+import { useEffectOnce } from "react-use";
 
 const UserList = (props: {
+  userIds: string[];
   onUserClick: (id: string, name: string) => void;
 }) => {
-  const [data, setData] = useState<User[]>([]);
+  const [users, setUsers] = useState<User[]>([]);
   useEffect(() => {
-    axios.get("/api/message/users").then((res) => {
-      console.log(res.data);
-      setData(res.data.list);
+    const newUsers: User[] = [];
+    const promises = props.userIds.map(async (id) => {
+      const res = await getUserInfo(id);
+      newUsers.push(res);
     });
-    return () => {
-      setData([]);
-    };
-  }, []);
+    Promise.all(promises).then(() => {
+      setUsers(newUsers);
+    });
+  }, [props.userIds]);
   return (
-    <List
-      dataSource={data}
-      renderItem={(item) => (
-        <List.Item
-          onClick={() => {
-            props.onUserClick(item.id, item.name);
-          }}
-          style={{ cursor: "pointer" }}
-        >
-          {item.name}
-        </List.Item>
-      )}
-    />
+    <div style={{ height: "calc(100% - 100)" }}>
+      <List
+        dataSource={users}
+        renderItem={(item: User) => (
+          <List.Item
+            onClick={() => {
+              props.onUserClick(item.userID, item.username);
+            }}
+            style={{ cursor: "pointer" }}
+          >
+            {item.username}
+          </List.Item>
+        )}
+      />
+    </div>
   );
 };
 
-const ChatView = (props: { userId: string; userName: string }) => {
-  const { messages, resetList } = useMessages();
-
-  useEffect(() => {
-    axios.get("/api/message/" + props.userId).then((res) => {
-      console.log(res.data);
-      resetList(
-        res.data.list.map(
-          (item: {
-            id: string;
-            type: string;
-            content: string;
-            userIdFrom: string;
-            userIdTo: string;
-          }) => {
-            return {
-              _id: item.id,
-              type: item.type,
-              content: item.content,
-              position: item.userIdFrom === props.userId ? "left" : "right",
-            };
-          }
-        )
-      );
-    });
-    return () => {
-      resetList([]);
-    };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [props.userId]);
-
+const ChatView = (props: {
+  messages: MessageProps[];
+  userId: string;
+  peerUserId: string;
+  peerUserName: string;
+  onSend?: (type: string, content: string) => void;
+}) => {
   const onSend = (type: string, content: string) => {
     console.log(type, content);
+    axios
+      .post("/api/messages", {
+        senderID: props.userId,
+        recipientID: props.peerUserId,
+        content: content,
+      })
+      .then(() => {
+        props.onSend?.(type, content);
+      });
   };
 
   const renderMessageContent = (message: MessageProps) => {
@@ -92,8 +85,8 @@ const ChatView = (props: { userId: string; userName: string }) => {
   return (
     <>
       <Chat
-        navbar={{ title: props.userName }}
-        messages={messages}
+        navbar={{ title: props.peerUserName }}
+        messages={props.messages}
         renderMessageContent={renderMessageContent}
         onSend={onSend}
       />
@@ -102,21 +95,129 @@ const ChatView = (props: { userId: string; userName: string }) => {
 };
 
 export const MessagePage = () => {
-  const [userId, setUserId] = useState<string>("");
-  const [userName, setUserName] = useState<string>("");
+  const navigate = useNavigate();
+
+  const [peerUserId, setPeerUserId] = useState<string>("");
+  const [peerUsername, setPeerUsername] = useState<string>("");
   const onUserClick = (id: string, name: string) => {
     console.log(id, name);
-    setUserId(id);
-    setUserName(name);
+    setPeerUserId(id);
+    setPeerUsername(name);
   };
+
+  const { user } = useAuth();
+  const { messages, resetList } = useMessages();
+  const [userIds, setUserIds] = useState<string[]>([]);
+  const [messageList, setMessageList] = useState<Message[]>([]);
+
+  const updateMessage = () => {
+    if (!user) {
+      return;
+    }
+    axios
+      .get("/api/messages", {
+        headers: {
+          testid: user?.userID || "",
+        },
+      })
+      .then((res) => {
+        setMessageList(res.data);
+        setUserIds(
+          Array.from(
+            new Set(
+              res.data.map((item: Message) => {
+                return item.senderID === user.userID
+                  ? item.recipientID
+                  : item.senderID;
+              })
+            )
+          )
+        );
+      });
+  };
+
+  useEffect(() => {
+    if (!user) {
+      navigate("/login");
+      return;
+    }
+
+    updateMessage();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [user]);
+
+  useEffect(() => {
+    if (!peerUserId || !user) {
+      return;
+    }
+    resetList(
+      messageList.map((item) => {
+        return {
+          _id: item.id,
+          type: "text",
+          content: { text: item.content },
+          position: item.senderID === user.userID ? "right" : "left",
+        };
+      })
+    );
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [peerUserId, user, messageList]);
+
+  const [modalVisible, setModalVisible] = useState<boolean>(false);
+
+  const newMessage = (values: { peerUserId: string }) => {
+    setUserIds([...userIds, values.peerUserId]);
+    setModalVisible(false);
+  };
+
+  const onSend = () => {
+    updateMessage();
+  };
+
+  useEffectOnce(() => {
+    const timer = setInterval(() => {
+      updateMessage();
+    }, 5000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  });
 
   return (
     <Layout style={{ height: "100%" }}>
       <Layout.Sider>
-        <UserList onUserClick={onUserClick} />
+        <Button
+          onClick={() => {
+            setModalVisible(true);
+          }}
+        >
+          新建会话
+        </Button>
+        <Modal
+          visible={modalVisible}
+          onCancel={() => {
+            setModalVisible(false);
+          }}
+          footer={null}
+        >
+          <Form onSubmit={newMessage}>
+            <Form.Input field="peerUserId" label="对方ID" />
+            <Button htmlType="submit">新建</Button>
+          </Form>
+        </Modal>
+        <UserList onUserClick={onUserClick} userIds={userIds} />
       </Layout.Sider>
       <Layout.Content>
-        {userId && <ChatView userId={userId} userName={userName} />}
+        {peerUserId && (
+          <ChatView
+            messages={messages}
+            userId={user?.userID ?? ""}
+            peerUserId={peerUserId}
+            peerUserName={peerUsername}
+            onSend={onSend}
+          />
+        )}
       </Layout.Content>
     </Layout>
   );
